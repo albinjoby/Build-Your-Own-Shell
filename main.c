14c3b76792df19096620e46e1b526b7733a83211
albinjoby <albinjobyponnu@gmail.com>
2026-01-29 09:40:04 +0530

    take user input

--- main.c
+++ main.c
@@ -4,7 +4,11 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-  printf("$");
+  char command[100];
+  while (1) {
+      printf("$ ");
+      fgets(command,sizeof(command),stdin);
+  }
 
   return 0;
 }
