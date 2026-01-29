29a01a02dcfda7cfbef6005e83016c7294c108c2
albinjoby <albinjobyponnu@gmail.com>
2026-01-29 09:49:53 +0530

    add echo

--- main.c
+++ main.c
@@ -10,11 +10,16 @@ int main(int argc, char *argv[]) {
   while (1) {
       printf("$ ");
       fgets(command,sizeof(command),stdin);
+      
       command[strcspn(command, "\n")] = '\0';
+      
       if (strcmp(command, "exit") == 0) {
           break;
+      }else if (strncmp(command, "echo", 4) == 0) {
+          printf("%s\n", command+5);
+      }else{
+          printf("%s: command not found\n",command);
       }
-      printf("%s: command not found\n",command);
   }
   return 0;
 }
